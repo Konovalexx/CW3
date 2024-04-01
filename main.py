2e53a2f1f5ce49e0d397e3d66c030840910afc11
Alex Con <konovalexx@gmail.com>
2024-04-01 21:19:28 +1000

    def sort_operations    date

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ def open_json_file():
     with open('operations.json', encoding='utf-8') as f:
         return json.load(f)
 
-def filter_operations(data):
+
+def filter_operations(operations_data):
     filtered_list = []
-    for operation in data:
-        if operation in data:
-            if operation.get('state') == 'EXECUTED':
+    for operation in operations_data:
+        if operation.get('state') == 'EXECUTED':
                 filtered_list.append(operation)
     return filtered_list
 
+def sort_operations(operations_data: list[dict]) -> list[dict]:
+    sorted_list = sorted(operations_data, key=lambda x: x['date'])
+    return sorted_list
+
+
 data = open_json_file()
 operations = filter_operations(data)
+operations = sort_operations(operations)
 print(operations)
