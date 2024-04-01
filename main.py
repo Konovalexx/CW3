5608af343e02a99f86e411860c014d8771b7a624
Alex Con <konovalexx@gmail.com>
2024-04-01 20:03:58 +1000

    EXECUTED operations function

--- main.py
+++ main.py
@@ -4,4 +4,14 @@ def open_json_file():
     with open('operations.json', encoding='utf-8') as f:
         return json.load(f)
 
-print(open_json_file())
+def filter_operations(data):
+    filtered_list = []
+    for operation in data:
+        if operation in data:
+            if operation.get('state') == 'EXECUTED':
+                filtered_list.append(operation)
+    return filtered_list
+
+data = open_json_file()
+operations = filter_operations(data)
+print(operations)
