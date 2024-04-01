da760c68160d783e6ddba3161a9e586828761025
Alex Con <konovalexx@gmail.com>
2024-04-02 00:58:48 +1000

    Docstings

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import json
 import datetime as dt
 
 def open_json_file():
+    """
+    Открывает и читает JSON файл 'operations.json', возвращая его содержимое в виде словаря.
+
+    Returns:
+        dict: Словарь с данными из файла 'operations.json'.
+    """
     with open('operations.json', encoding='utf-8') as f:
         return json.load(f)
 
 def filter_operations(operations_data):
+    """
+    Фильтрует список операций, оставляя только те, у которых состояние равно 'EXECUTED'.
+
+    Args:
+        operations_data (list): Список операций.
+
+    Returns:
+        list: Фильтрованный список операций.
+    """
     filtered_list = []
     for operation in operations_data:
         if operation.get('state') == 'EXECUTED':
@@ -13,10 +28,28 @@ def filter_operations(operations_data):
     return filtered_list
 
 def sort_operations(operations_data: list[dict]) -> list[dict]:
+    """
+    Сортирует список операций по дате в обратном порядке.
+
+    Args:
+        operations_data (list[dict]): Список операций.
+
+    Returns:
+        list[dict]: Отсортированный список операций.
+    """
     sorted_list = sorted(operations_data, key=lambda x: x['date'], reverse=True)
     return sorted_list
 
 def mask_operation_info(operation):
+    """
+    Маскирует информацию об операции, скрывая часть номера счета.
+
+    Args:
+        operation (dict): Словарь с информацией об операции.
+
+    Returns:
+        str: Маскированная информация об операции.
+    """
     operation_from = operation.get('from')
     operation_to = operation.get('to')
 
@@ -30,6 +63,15 @@ def mask_operation_info(operation):
             return f'Счет **{numbers[-4:]}'
 
 def format_date(operations):
+    """
+    Форматирует дату операции в формат "dd.mm.yyyy".
+
+    Args:
+        operations (dict): Словарь с информацией об операции.
+
+    Returns:
+        str: Форматированная дата операции.
+    """
     date = operations['date']
     dt_time = dt.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
     return dt_time.strftime("%d.%m.%Y")
